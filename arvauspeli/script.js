c9cf94d47634ce323ed8a5b638d97ae34fc484b5
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-10-10 13:36:54 +0300

    arvauspeli

---
+++ arvauspeli/script.js
@@ -0,0 +1,47 @@
+
+var arvauskerrat = 0;
+var voitot = 0;
+var haviot = 0;
+var arvattavaNumero = Math.floor(Math.random() * 10) + 1
+
+window.onload = function taulukonTulostus() {
+  var taulukko = "";
+
+  for (let i = 1; i <= 10; i++) {
+    taulukko += "<button type='button' class='napit' name='napit' onclick='arvaus(this)' value='" + i + "' >" + i + "</button>"
+  /*  if (i % 3 == 0) {
+      taulukko += "<br>"
+    }*/
+
+  }
+  document.getElementById('napit').innerHTML = taulukko;
+}
+
+function arvaus(numero) {
+  var arvattuNumero = numero.value;
+  console.log(arvattuNumero, arvattavaNumero)
+
+  if (arvauskerrat < 3) {
+    if (arvattuNumero < arvattavaNumero) {
+      alert("numero on isompi")
+      arvauskerrat++
+    } else if (arvattuNumero > arvattavaNumero) {
+      alert("numero on pienempi")
+      arvauskerrat++
+    } else if (arvattuNumero == arvattavaNumero){
+      alert("OIKEIN!")
+      arvauskerrat = 0;
+      arvattavaNumero = Math.floor(Math.random() * 10) + 1
+      voitot++
+      document.getElementById('voitot').innerHTML = "Voitot: " + voitot;
+    }
+  }
+
+  if (arvauskerrat == 3) {
+    alert("Arvaukset loppuivat, hävisit!")
+    arvattavaNumero = Math.floor(Math.random() * 10) + 1
+    haviot++
+    document.getElementById('haviot').innerHTML = "Häviöt: " + haviot;
+    arvauskerrat = 0;
+  }
+}
