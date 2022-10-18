5b3fad1865286502d3a9c6a365df7f4e6ab77c8c
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-10-18 15:38:06 +0300

    böö

---
+++ muistipeli/script.js
@@ -0,0 +1,12 @@
+
+
+function naytaKortti() {
+
+var kortti = document.getElementById('kortti_1');
+
+  if (kortti.src = "imgs/kuva_abc.png") {
+    document.getElementById('kortti_1').src = "imgs/kuva1.png";
+
+  }
+  kortti.toggle();
+}
