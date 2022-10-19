f43b7b1cbcd3b639096fdf3bf4b84f578a3ba51c
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-10-19 23:29:07 +0300

    js createElement

---
+++ harjoitukset/jsCreateElement/script.js
@@ -0,0 +1,56 @@
+const lisaaBtn3 = document.querySelector(".btn-add3");
+const lisaaBtn4 = document.querySelector(".btn-add4");
+const container1 = document.getElementById("container_1");
+const container2 = document.getElementById("container_2");
+
+
+lisaaBtn3.addEventListener("click", addNew3)
+lisaaBtn4.addEventListener("click", addNew4)
+
+  function addNew3() {
+
+    for(let i = 0; i < 9; i++) {
+
+      var newDiv3 = document.createElement("div");
+      newDiv3.id = "new_div" + i;
+      container1.appendChild(newDiv3);
+      newDiv3.classList.add("kortti_div");
+    }
+  }
+
+
+
+
+
+
+
+
+  function addNew4() {
+
+    for(let j = 0; j < 16; j++) {
+
+      var newDiv4 = document.createElement("div");
+      newDiv4.id = "new_div" + j;
+      container2.appendChild(newDiv4);
+      newDiv4.classList.add("kortti_div");
+
+    }
+
+  };
+
+
+
+
+
+
+
+// Toinen tapa luoda funktio eventListernerillä
+
+/*
+lisaaNappi.addEventListener("click", function() {
+
+  var newDiv = document.createElement("div");
+  container.appendChild(newDiv);
+  newDiv.classList.add("kortti_div");
+});
+*/
